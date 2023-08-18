76cc619569dcfcb5b789e1b8e5fba61e6fde1dc0
farrelarrizal <72312914+farrelarrizal@users.noreply.github.com>
2023-08-18 20:03:53 +0700

    Update iris-frontend.py

--- src/iris-frontend.py
+++ src/iris-frontend.py
@@ -6,7 +6,7 @@ import requests
 image = Image.open('assets/header-iris.png')
 st.image(image)
 st.title("Iris Classifier App")
-st.markdown('by: Farrel Arrizal')
+st.markdown('by: Farrel Arrizal | Batch Period: JUL23')
 st.divider()
 st.subheader('Just type the value. Then, click the Predict button to get the result :sunglasses:')
 
